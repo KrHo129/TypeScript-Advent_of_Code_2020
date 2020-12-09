3196e04a94d9be9500aeceb4ec1d4ff508a9d119
KrHo <krhodev@gmail.com>
2020-12-09 18:24:04 +0100

    some commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AoC-y2020';
+export class AppComponent implements OnInit {
+  days: string[] = [];
+
+
+  ngOnInit() {
+    for (let i = 1; i < 26; i++) {
+      this.days.push(("0" + i).slice(-2))
+    }
+  }
+
 }
