61b05fa42401f9a7a4b92c3c68883067237aca37
KrHo <krhodev@gmail.com>
2020-12-16 20:56:27 +0100

    day 14

--- src/app/day14/d14-p01.service.ts
+++ src/app/day14/d14-p01.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@angular/core';
 import { TimerService } from '../shared/timer.service';
 import { D14SharedService } from './d14-shared.service';
 
+interface MaskValue {
+  position: number;
+  value: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -14,14 +19,72 @@ export class D14P01Service {
   getResult(rawInput: string): { result: string; calculationTime: number } {
     this.timerService.startTimer();
 
-    const input = this.sharedService.getParsedInput(rawInput);
+    const commands = this.sharedService.getParsedInput(rawInput);
 
-    const result = null;
+    let mask: string;
+    const memory: { [s: string]: string } = {};
+
+    for (let command of commands) {
+      if (command.commandType === 'changeMask') {
+        mask = command.maskValue;
+      } else if (command.commandType === 'updateMemory') {
+        const newMemoryValue = this.getMemoryValue(
+          command.newMemoryValue,
+          mask
+        );
+        memory[command.memoryLocation] = newMemoryValue;
+      }
+    }
+
+    let sum = 0;
+    for (let value of Object.values(memory)) {
+      sum += parseInt(value, 2);
+    }
 
     const calculationTime = this.timerService.getTime();
     return {
-      result: result.toString(),
+      result: sum.toString(),
       calculationTime: calculationTime,
     };
+  }
+
+  private getMemoryValue(numericValue: number, mask: string): string {
+    let bitValue = numericValue.toString(2);
+
+    for (let i = bitValue.length; i < 36; i++) {
+      bitValue = '0' + bitValue;
+    }
+
+    const maskValues = this.getParsedMask(mask);
+
+    for (let maskValue of maskValues) {
+      bitValue = this.replaceAt(bitValue, maskValue.position, maskValue.value);
+    }
+
+    return bitValue;
+  }
+
+  private getParsedMask(mask: string): MaskValue[] {
+    const parsedMask: MaskValue[] = [];
+    for (let i = 0; i < mask.length; i++) {
+      if (mask[i] === 'X') {
+        continue;
+      }
+      const maskVal: MaskValue = {
+        position: i,
+        value: mask[i],
+      };
+      parsedMask.push(maskVal);
+    }
+
+    return parsedMask;
+  }
+
+  replaceAt(string: string, index: number, replacement: string) {
+    return (
+      string.substr(0, index) +
+      replacement +
+      string.substr(index + replacement.length)
+    );
   }
 }
