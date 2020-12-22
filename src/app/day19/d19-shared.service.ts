28365d0904cb14d031825a05e4fb47b244d7c7ef
KrHo <krhodev@gmail.com>
2020-12-22 13:52:47 +0100

    day 19

--- src/app/day19/d19-shared.service.ts
+++ src/app/day19/d19-shared.service.ts
@@ -36,38 +36,4 @@ export class D19SharedService {
       rules: rules,
     };
   }
-
-  // getParsedInputPart2(rawInput: string): { messages: string[]; rules: Rules } {
-  //   const sections = rawInput.split('\n\n');
-
-  //   const messages = sections[1].split('\n');
-
-  //   const rules = {};
-
-  //   for (let line of sections[0].split('\n')) {
-  //     const splittedLine = line.split(': ');
-  //     const key = splittedLine[0];
-
-  //     let ruleStr = splittedLine[1];
-  //     if (key === '8') {
-  //       ruleStr = '42 | 42 8';
-  //     } else if (key === '11') {
-  //       ruleStr = '42 31 | 42 11 31';
-  //     }
-
-  //     const ruleArr = ruleStr.split(' | ');
-  //     let parsedRule = [];
-
-  //     for (let rule of ruleArr) {
-  //       const singleRule = rule.split(' ');
-  //       parsedRule.push(singleRule);
-  //     }
-  //     rules[key] = parsedRule;
-  //   }
-
-  //   return {
-  //     messages: messages,
-  //     rules: rules,
-  //   };
-  // }
 }
