ba510be0f4ee4685e204fb7c0c7902aaadb1046e
KrHo <krhodev@gmail.com>
2020-12-28 17:08:48 +0100

    day 22

--- src/app/day22/d22-input.service.ts
+++ src/app/day22/d22-input.service.ts
@@ -4,14 +4,88 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class D22InputService {
-  inputStr = ``;
+  inputStr = `Player 1:
+41
+48
+12
+6
+1
+25
+47
+43
+4
+35
+10
+13
+23
+39
+22
+28
+44
+42
+32
+31
+24
+50
+34
+29
+14
 
-  testInput = ``;
+Player 2:
+36
+49
+11
+16
+20
+17
+26
+30
+18
+5
+2
+38
+7
+27
+21
+9
+19
+15
+8
+45
+37
+40
+33
+46
+3`;
+
+  testInput = `Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10`;
+
+  testInputLoop = `Player 1:
+43
+19
+
+Player 2:
+2
+29
+14`;
 
   constructor() {}
 
   getRawInput(): string {
     return this.inputStr;
     // return this.testInput;
+    return this.testInputLoop;
   }
 }
--- src/app/day22/d22-p01.service.ts
+++ src/app/day22/d22-p01.service.ts
@@ -16,9 +16,31 @@ export class D22P01Service {
 
     const input = this.sharedService.getParsedInput(rawInput);
 
+    const player1 = input[0];
+    const player2 = input[1];
+
+    while (true) {
+      if (!player1.hasCards() || !player2.hasCards()) {
+        break;
+      }
+
+      const player1Card = player1.getTopCard();
+      const player2Card = player2.getTopCard();
+
+      if (player1Card > player2Card) {
+        player1.addCard(player1Card);
+        player1.addCard(player2Card);
+      } else {
+        player2.addCard(player2Card);
+        player2.addCard(player1Card);
+      }
+    }
+
+    const result = player1.getCardsScore() + player2.getCardsScore();
+
     const calculationTime = this.timerService.getTime();
     return {
-      result: 'null'.toString(),
+      result: result.toString(),
       calculationTime: calculationTime,
     };
   }
